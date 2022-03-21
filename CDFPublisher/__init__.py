6b46bc4decbe29c478bf0629df7e7718293c5edb
Murad Sæter <murad.sater@cognite.com>
2022-03-21 12:53:13 +0100

    Add extractor pipeline status

--- CDFPublisher/__init__.py
+++ CDFPublisher/__init__.py
@@ -6,10 +6,11 @@ from datetime import datetime
 
 import azure.functions as func
 from cognite.client import CogniteClient
-from cognite.client.data_classes import TimeSeries
+from cognite.client.data_classes import TimeSeries, ExtractionPipelineRun
 
 ts_cache = set()
-
+cognite_client = None
+EXTRACTOR_PIPELINE_XID = os.getenv("EXTRACTOR_PIPELINE_EXTERNAL_ID", "function_app")
 
 def create_cognite_client():
 
@@ -25,7 +26,7 @@ def create_cognite_client():
 
     logging.debug(f"token_url={TOKEN_URL}, token_client_id={CLIENT_ID}, token_client_secret=(hidden), token_scopes={SCOPES}, project={COGNITE_PROJECT}, base_url={BASE_URL}")
 
-    return CogniteClient(
+    client = CogniteClient(
         token_url=TOKEN_URL,
         token_client_id=CLIENT_ID,
         token_client_secret=CLIENT_SECRET,
@@ -33,7 +34,11 @@ def create_cognite_client():
         project=COGNITE_PROJECT,
         base_url=BASE_URL,
         client_name="azure_function_publisher",
-    ) 
+    )
+
+    client.extraction_pipeline_runs.create(ExtractionPipelineRun(status="success", external_id=EXTRACTOR_PIPELINE_XID))
+
+    return client
 
 def add_dp(msg, datapoints):
     #{'NodeId': 'http://www.prosysopc.com/OPCUA/SimulationNodes/#i=1006', 'ApplicationUri': 'urn:demo-opcua.mqizqljncwouhpx5ypmrxna13b.fx.internal.cloudapp.net:OPCUA:SimulationServer', 'Value': {'Value': -1.333333, 'SourceTimestamp': '2022-03-15T22:10:20Z'}}
@@ -53,20 +58,39 @@ def get_dp(datapoints):
 
 def main(event: func.EventGridEvent):
     global ts_cache
-
-    client = create_cognite_client()
+    global cognite_client
+    
+    if not cognite_client:
+        cognite_client = create_cognite_client()
 
     messages = json.loads(base64.b64decode(event.get_json()['body']))
+
     datapoints = {}
     for msg in messages:
         xid = msg['NodeId']
         if not xid in ts_cache:
-            if client.time_series.retrieve(external_id=xid) == None:
-                client.time_series.create(TimeSeries(name=xid, external_id=xid, is_string=False))
-            ts_cache.add(xid)
+            cur_ts = cognite_client.time_series.retrieve(external_id=xid)
+            if cur_ts == None:
+                if 'DisplayName' in msg:
+                    cognite_client.time_series.create(TimeSeries(name=msg['DisplayName'], external_id=xid, is_string=False))
+                    ts_cache.add(xid)
+                else:
+                    cognite_client.time_series.create(TimeSeries(name=xid, external_id=xid, is_string=False))
+            else:
+                ts_cache.add(xid)
 
         add_dp(msg, datapoints)
 
 
     if len(datapoints) > 0:
-        client.datapoints.insert_multiple(get_dp(datapoints))
+        try:
+            cognite_client.datapoints.insert_multiple(get_dp(datapoints))
+        except:
+            cognite_client = create_cognite_client()
+            cognite_client.datapoints.insert_multiple(get_dp(datapoints))
+
+    try:
+        cognite_client.extraction_pipeline_runs.create(ExtractionPipelineRun(status="seen", external_id=EXTRACTOR_PIPELINE_XID))
+    except:
+        cognite_client = create_cognite_client()
+        cognite_client.extraction_pipeline_runs.create(ExtractionPipelineRun(status="seen", external_id=EXTRACTOR_PIPELINE_XID))
